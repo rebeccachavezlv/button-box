476055ceb34e8f1d397f7d74c9a36a602ea89430
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2021-10-03 18:24:47 -0700

    created separated form input component with styling

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './sign-in.styles.scss'
+
+import FormInput from '../form-input/form-input.component'
+
+class SignIn extends React.Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({ [name]: value })
+    }
+
+    render(){
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput name='email' label='Email' type='email' value={this.state.email} handleChange={this.handleChange} required />
+                    <FormInput name='password' label='Password' type='password' value={this.state.password} handleChange={this.handleChange} required />
+                    <input type='submit' value='Submit Form' />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn
